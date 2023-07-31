611c6a83d0abeab472ee9b847a3fed647a20825f
benny <benyaminchor@gmail.com>
2023-07-31 20:39:29 +0300

    The code runs, before valgrind

---
+++ db_struct.h
@@ -0,0 +1,35 @@
+#ifndef CHECKPOINT_BINYA2_DB_STRUCT_H
+#define CHECKPOINT_BINYA2_DB_STRUCT_H
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#define MAX_NAME_LENGTH 50
+#define MAX_PHONE_LENGTH 11
+#define SCORE_COUNT 10
+
+typedef struct {
+    char _firstName[MAX_NAME_LENGTH], _lastName[MAX_NAME_LENGTH], _phoneNumber[MAX_PHONE_LENGTH];
+    int _grade, _class, _scores[SCORE_COUNT];
+    struct Student* _next;
+} Student;
+
+typedef struct {
+    Student *studentHead;
+} Class;
+
+typedef struct {
+    Class classes[10];
+} Grade;
+
+typedef struct {
+    Grade grade[12];
+} School;
+
+void initGrade(Grade* grade);
+void initSchool(School* school);
+Student* initStudent(char* line);
+void add_student(Class* class, Student* new_student);
+void print_student(School* school);
+void free_student(School* school);
+
+#endif //CHECKPOINT_BINYA2_DB_STRUCT_H
--- main.c
+++ main.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
+#include "db_struct.h"
 
 int main() {
     FILE *file;
     char buffer[100];
 
-    file = fopen("C:\\exselentim\\bootcamp\\checkpoint-binya2\\students_with_class.txt", "r");
+    file = fopen("/home/benny/exelenteam/checkpoint-binya2/students_with_class.txt", "r");
 
     if (file == NULL) {
         printf("Error opening the file.\n");
         return 1;
     }
-
+    School school;
+    initSchool(&school);
+    Student* new_student = NULL;
     while (fgets(buffer, sizeof(buffer), file) != NULL) {
-        printf("%s", buffer);
+        new_student = initStudent(buffer);
+        add_student(&school.grade[new_student->_grade - 1].classes[new_student->_class - 1], new_student);
     }
-
     fclose(file);
-
+    print_student(&school);
+    free_student(&school);
     return 0;
 }
